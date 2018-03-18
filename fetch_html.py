ec2942d917ae8d81d4e388fed8dea287938c6723
Shashwat Chandra <chandra.shashwat@gmail.com>
2018-03-18 09:58:07 +0530

    Modularising. Adding code to fetch html

---
+++ fetch_html.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup as BS
+import requests
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+
+def parse_html(text):
+  soup = BS(text, "html5lib")
+
+  for tag in soup(['script', 'style']):
+    tag.decompose()
+
+  for tag in soup.find_all(True, string=lambda x: True):
+    # Adding spaces around all text strings
+    if tag.string is not None:
+      tag.string = ' ' + tag.string + ' '
+
+  return " ".join(soup.get_text().strip().split())
+
+def get_html(url):
+  r = requests.get(url)
+  if r.status_code != 200:
+    raise Exception("Error with reading url")
+
+  return parse_html(r.text)
+
+def get_headless_html(url, wait=2):
+  driver = webdriver.Chrome(chrome_options=options)
+  driver.get(url)
+  driver.implicitly_wait(wait)
+  src = driver.page_source()
+  driver.quit()
+
+  return parse_html(src)
+
--- read_chrome.py
+++ read_chrome.py
@@ -1,50 +1,54 @@
 import sqlite3
 import os
 import sys
-import tempfile
-import shutil
-import datetime
+from tempfile import NamedTemporaryFile
+from shutil import copyfile
+from datetime import datetime, timedelta
 
 def to_google_time(time):
-  return (time - datetime.datetime(1601, 1, 1)).total_seconds() * 1_000_000
+  return (time - datetime(1601, 1, 1)).total_seconds() * 1_000_000
 
-file_name = os.path.expanduser('~/Library/Application Support/Google/Chrome/Default/History')
-if len(sys.argv) >= 2:
-  file_name = sys.argv[1]
+def get_links(start_time = None, limit = 10, offset = 0, history_file_path = None, ascending = False):
+  if history_file_path is None:
+    # TODO: Make this OS-Specific
+    history_file_path = os.path.expanduser('~/Library/Application Support/Google/Chrome/Default/History')
 
-if not os.path.isfile(file_name):
-  print ("File doesn't exist")
-  sys.exit(1)
+  if not os.path.isfile(history_file_path):
+    raise IOError("File doesn't exist")
+    sys.exit(1)
 
-print ("--- Copying DB file")
-tf = tempfile.NamedTemporaryFile(delete=False)
-shutil.copyfile(file_name, tf.name)
+  print ("--- Copying DB file")
+  tf = NamedTemporaryFile(delete=False)
+  copyfile(history_file_path, tf.name)
 
-print ("--- Creating DB Connection")
-conn = sqlite3.connect(tf.name)
+  print ("--- Creating DB Connection")
+  conn = sqlite3.connect(tf.name)
 
-print ('--- Query')
-limit = 10
-offset = 0
-ascending = False
-latest_timestamp = datetime.datetime.now() - datetime.timedelta(1)
+  print ('--- Query')
+  if start_time is None:
+    start_time = datetime.now() - timedelta(1)
 
-print ('Select at most {} visits after {}'.format(limit, latest_timestamp))
+  print ('Select at most {} visits after {}'.format(limit, start_time))
 
-cur = conn.execute("SELECT COUNT(*) FROM urls WHERE last_visit_time > {}".format(to_google_time(latest_timestamp)))
-print("{} total entries".format(cur.fetchall()[0][0]))
-print()
+  cur = conn.execute("SELECT COUNT(*) FROM urls WHERE last_visit_time > {}".format(to_google_time(start_time)))
+  print("{} total entries".format(cur.fetchall()[0][0]))
+  print()
 
-for row in conn.execute("""
-SELECT url, title, datetime(last_visit_time / 1000000 + (strftime('%s', '1601-01-01')), 'unixepoch')
-FROM urls
-WHERE last_visit_time > {time}
-ORDER BY last_visit_time {dir}
-LIMIT {limit} OFFSET {offset}
-""".format(time=to_google_time(latest_timestamp), limit=limit, offset=offset, dir=("ASC" if ascending else "DESC"))):
-  print (row)
+  rows = []
+  cur = conn.execute("""
+  SELECT url, title, datetime(last_visit_time / 1000000 + (strftime('%s', '1601-01-01')), 'unixepoch') AS t
+  FROM urls
+  WHERE last_visit_time > {time}
+  ORDER BY last_visit_time {dir}
+  LIMIT {limit} OFFSET {offset}
+  """.format(time=to_google_time(start_time), limit=limit, offset=offset, dir=("ASC" if ascending else "DESC"))):
+    print (row)
+    rows.append(dict(zip(["url", "title", "time"], row)))
 
-print ("Finishing")
-conn.close()
-tf.close()
-os.remove(tf.name)
+
+  print ("--- Finishing")
+  conn.close()
+  tf.close()
+  os.remove(tf.name)
+
+  return rows
